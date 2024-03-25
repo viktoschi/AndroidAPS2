2cfa01fcc1c8a099dbdf53183bbe60f3f8d5eaa3
vicky <a>
2024-03-25 04:22:26 +0100

    push limits

--- core/interfaces/src/main/kotlin/app/aaps/core/interfaces/utils/HardLimits.kt
+++ core/interfaces/src/main/kotlin/app/aaps/core/interfaces/utils/HardLimits.kt
@@ -5,14 +5,14 @@ interface HardLimits {
 
         // Very Hard Limits Ranges
         // First value is the Lowest and second value is the Highest a Limit can define
-        val VERY_HARD_LIMIT_MIN_BG = doubleArrayOf(80.0, 180.0)
+        val VERY_HARD_LIMIT_MIN_BG = doubleArrayOf(70.0, 180.0)
         val VERY_HARD_LIMIT_MAX_BG = doubleArrayOf(90.0, 200.0)
-        val VERY_HARD_LIMIT_TARGET_BG = doubleArrayOf(80.0, 200.0)
+        val VERY_HARD_LIMIT_TARGET_BG = doubleArrayOf(70.0, 200.0)
 
         // Very Hard Limits Ranges for Temp Targets
-        val VERY_HARD_LIMIT_TEMP_MIN_BG = intArrayOf(72, 180)
-        val VERY_HARD_LIMIT_TEMP_MAX_BG = intArrayOf(72, 270)
-        val VERY_HARD_LIMIT_TEMP_TARGET_BG = intArrayOf(72, 200)
+        val VERY_HARD_LIMIT_TEMP_MIN_BG = intArrayOf(62, 180)
+        val VERY_HARD_LIMIT_TEMP_MAX_BG = intArrayOf(62, 270)
+        val VERY_HARD_LIMIT_TEMP_TARGET_BG = intArrayOf(62, 200)
         val MIN_DIA = doubleArrayOf(5.0, 5.0, 5.0, 5.0, 5.0)
         val MAX_DIA = doubleArrayOf(9.0, 9.0, 9.0, 9.0, 10.0)
         val MIN_IC = doubleArrayOf(2.0, 2.0, 2.0, 2.0, 0.3)
